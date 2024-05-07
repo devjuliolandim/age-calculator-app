0a61689945173ada78856ebacb2209387f9e1f7a
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-07 10:51:14 -0300

    fill verification

--- js/index.js
+++ js/index.js
@@ -1,16 +1,39 @@
 function showResults(){
     
-    const DAY = parseInt(document.querySelector("#day").value);
-    const MONTH = parseInt(document.querySelector("#month").value);
-    const YEAR = parseInt(document.querySelector("#day").value);
+    const DAY = document.querySelector("#day").value;
+    const MONTH = document.querySelector("#month").value;
+    const YEAR = document.querySelector("#year").value;
+
+    const INPUTS = Array.from(document.querySelectorAll('input'));
+    const LABELS = Array.from(document.querySelectorAll('.input-upper-text'));
+    const FILL = Array.from(document.querySelectorAll('.fill-forget'));
+
+    
+
+    areThoseFilled(INPUTS, LABELS, FILL);
 
     areThoseValid(DAY, MONTH, YEAR);
     
 }
 
-function areThoseValid(day, month, year){
+function areThoseFilled(inputs, labels, fill) {
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === '') {
+            inputs[i].classList.add('wrong-border');
+            labels[i].classList.add('wrong');
+            fill[i].textContent = "Don't forget to fill this info";
+        } else {
+            inputs[i].classList.remove('wrong-border');
+            labels[i].classList.remove('wrong');
+            fill[i].textContent = "";
+        }
+    }
+}
 
+function areThoseValid(day, month, year){
+    
     var currentYear = new Date().getFullYear;
+
 
     if(day <= 0 || day >=31 || month <= 0 || month>12 || year<1900 || year>= currentYear){
         
