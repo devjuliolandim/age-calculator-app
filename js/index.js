f12701944ef7f98fc2eb077764f6bd9ea0f0e34d
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-07 11:04:45 -0300

    changed fill to fillMessage

--- js/index.js
+++ js/index.js
@@ -6,27 +6,40 @@ function showResults(){
 
     const INPUTS = Array.from(document.querySelectorAll('input'));
     const LABELS = Array.from(document.querySelectorAll('.input-upper-text'));
-    const FILL = Array.from(document.querySelectorAll('.fill-forget'));
+    const FILL_MESSAGE = Array.from(document.querySelectorAll('.fill-forget'));
 
     
 
-    areThoseFilled(INPUTS, LABELS, FILL);
+    
 
-    areThoseValid(DAY, MONTH, YEAR);
+    if(areThoseFilled(INPUTS,LABELS,FILL_MESSAGE)){
+        areThoseValid(DAY,MONTH,YEAR)
+    }
     
 }
 
-function areThoseFilled(inputs, labels, fill) {
+function areThoseFilled(inputs, labels, fillMessage) {
+
+    var count = 0;
+
     for (var i = 0; i < inputs.length; i++) {
         if (inputs[i].value === '') {
             inputs[i].classList.add('wrong-border');
             labels[i].classList.add('wrong');
-            fill[i].textContent = "Don't forget to fill this info";
+            fillMessage[i].textContent = "Don't forget to fill this info";
+           
         } else {
             inputs[i].classList.remove('wrong-border');
             labels[i].classList.remove('wrong');
-            fill[i].textContent = "";
+            fillMessage[i].textContent = ""; 
+            count++;
         }
+    }
+
+    if(count == inputs.length){
+        return true;
+    }else{
+        return false;
     }
 }
 
@@ -36,6 +49,10 @@ function areThoseValid(day, month, year){
 
 
     if(day <= 0 || day >=31 || month <= 0 || month>12 || year<1900 || year>= currentYear){
-        
+        alert('fuck');
     }
+}
+
+function error(inputs, labels, fillMessage){
+
 }
